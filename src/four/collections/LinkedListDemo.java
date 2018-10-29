9ec94807b3c63426fdebb7f2d72d751870fb1ae3
Mobilnik <iaulitin@yandex.ru>
2018-10-30 00:01:18 +0300

    Обновил до try с ресурсами. Разобрали.

--- src/four/collections/LinkedListDemo.java
+++ src/four/collections/LinkedListDemo.java
@@ -2,7 +2,6 @@ package four.collections;
 
 import animals.Animal;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
--- src/six/sql/StatementDemo.java
+++ src/six/sql/StatementDemo.java
@@ -5,14 +5,13 @@ import java.sql.*;
 public class StatementDemo {
 
     public static void main(String[] args) throws SQLException {
-        Connection conn = null;
         //пример с русскоязычной википедии
-        try {
-            Class.forName("org.postgresql.Driver");
 
-            conn = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/sample_database",
-                    "postgres", "postgres");
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/sample_database",
+                "postgres", "postgres");
+             Statement stmt = conn.createStatement()) {
+            Class.forName("org.postgresql.Driver");
 
             if (conn == null) {
                 System.out.println("Нет соединения с БД!");
@@ -20,10 +19,10 @@ public class StatementDemo {
             }
 
             //Statement служит для исполнения запросов к БД
-            Statement stmt = conn.createStatement();
+
             //Result Set - набор строк из БД.
             ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-            //rs.next() - работает не как итератор,
+            //rs.next() - работает не как итератор
             while (rs.next()) {
                 System.out.println(
                         rs.getRow() + ". "
@@ -35,14 +34,9 @@ public class StatementDemo {
 
             // При закрытии Statement автоматически закрываются
             // все связанные с ним открытые объекты ResultSet
-            stmt.close();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-        } finally {
-            // обратить внимание, для чего используется блок finally
-            if (conn != null) {
-                conn.close();
-            }
         }
+        // обратить внимание, для чего используется блок finally
     }
 }
